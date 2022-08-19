eee42d14b58081516bfa6f49e636370f7e02cfdb
Mohamed Elkazzaz <m.elkazzaz96@gmail.com>
2022-08-19 13:18:42 +0200

    pharos assigment

---
+++ Pharos Assigment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pharos Assigment
+//
+//  Created by Mohamed Elkazzaz on 19/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
